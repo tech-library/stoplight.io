776bb047ed15b5869c94cbb4e2929669e9accc27
Marc MacLeod <marbemac@gmail.com>
2018-09-30 18:42:29 -0500

    whoops body -> description

--- src/components/ImageSection/index.js
+++ src/components/ImageSection/index.js
@@ -5,14 +5,14 @@ import Section from '../Section';
 
 import { colors, sizes, Button } from '@stoplight/ui';
 
-const ImageSection = ({ title, body, image, rootClassName }) => {
+const ImageSection = ({ title, description, image, rootClassName }) => {
   return (
     <Section rootClassName={cn(rootClassName, 'flex')} paddingClassName={'pt-48 pb-40'}>
       <div className="flex flex-col flex-1 w-1/2 md:w-100 text-right items-end pr-20">
         <h2 className="max-w-xs text-secondary text-right mb-10">{title}</h2>
 
         <div className="mb-12 pb-12 max-w-md leading-loose text-lg border-b border-darken-50">
-          {body}
+          {description}
         </div>
 
         <a href="https://next.stoplight.io/join">
