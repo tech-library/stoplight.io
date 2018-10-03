60a4be36a0985deeed3ff6b0c98de2c5e554c0d9
Marc MacLeod <marbemac@gmail.com>
2018-10-02 19:55:20 -0500

    tree shaking for font awesome

--- src/utils/fontawesome/index.js
+++ src/utils/fontawesome/index.js
@@ -1,21 +1,23 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faPaintBrush,
-  faBook,
-  faFlask,
-  faServer,
-  faCheck,
-  faCheckCircle,
-  faRss,
-  faUsers,
-  faComments,
-  faGraduationCap,
-  faSuitcase,
-  faArrowRight,
-  faBars,
-  faTimes,
-} from '@fortawesome/free-solid-svg-icons';
-import { faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+import { faPaintBrush } from '@fortawesome/free-solid-svg-icons/faPaintBrush';
+import { faBook } from '@fortawesome/free-solid-svg-icons/faBook';
+import { faFlask } from '@fortawesome/free-solid-svg-icons/faFlask';
+import { faServer } from '@fortawesome/free-solid-svg-icons/faServer';
+import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons/faCheckCircle';
+import { faRss } from '@fortawesome/free-solid-svg-icons/faRss';
+import { faUsers } from '@fortawesome/free-solid-svg-icons/faUsers';
+import { faComments } from '@fortawesome/free-solid-svg-icons/faComments';
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons/faGraduationCap';
+import { faSuitcase } from '@fortawesome/free-solid-svg-icons/faSuitcase';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight';
+import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
+import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
+
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 
 export const init = () => {
   library.add(faPaintBrush);
